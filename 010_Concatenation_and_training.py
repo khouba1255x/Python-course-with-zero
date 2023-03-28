bf07ed55b1ac5beb6eff9d4f1d7f3524ac9f3d2b
khoubaib <zombie.1255x@gmail.com>
2023-03-28 10:18:06 +0100

    The vd nomber 10

---
+++ 010_Concatenation_and_training.py
@@ -0,0 +1,18 @@
+msg = " I Love"
+lang = "Python"
+print(msg + " " + lang)
+
+full = msg + " " + lang
+print(full)
+
+a = "First \
+second \
+third"
+
+b = "A \
+B \
+C"
+
+print(a + "\n " + b)
+print("hello\n world")
+print("hello" + 1)
