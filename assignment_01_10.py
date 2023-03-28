7abb8a192759312fca92a32f61049e01cd1223ed
khoubaib <zombie.1255x@gmail.com>
2023-03-28 12:15:35 +0100

    The first assigment

---
+++ assignment_01_10.py
@@ -0,0 +1,17 @@
+# this is the first assignment from video 1 to video 10
+my_name = "Khoubaib"
+my_age = "17"
+my_country = "Tunisia"
+
+print('"Name: ' + my_name + '"')
+print('"Age: ' + my_age + '"')
+print('"Country: ' + my_country + '"')
+
+
+print('Hello, My name is ' + my_name + ' and Iam '
+      + my_age + ' Years old I live in ' + my_country)
+
+
+print(type(my_name))
+print(type(my_age))
+print(type(my_country))
