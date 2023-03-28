db550d7e930f736279ea2e6845c1ccfea1803785
khoubaib <zombie.1255x@gmail.com>
2023-03-28 10:44:39 +0100

    The vd nomber 11

---
+++ 011_Strings.py
@@ -0,0 +1,21 @@
+myStringOne = 'This is single Quote'
+myStringTwo = "This is Double Quotes"
+
+print(myStringOne)
+print(myStringTwo)
+
+myStringThree = 'This is single Quote "test"'
+myStringFour = "This is single Quote 'test'"
+
+print(myStringThree)
+print(myStringFour)
+
+myStringFive = '''first
+second 'Test' ='Test'
+third'''
+myStringSix = """first
+second "Test"  "Test" 
+third"""
+
+print(myStringFive)
+print(myStringSix)
