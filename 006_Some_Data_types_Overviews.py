87e255ad5756711620ae1f78295684d1fd9b162e
khoubaib <zombie.1255x@gmail.com>
2023-03-28 09:22:44 +0100

    The vd nomber 6

---
+++ 006_Some_Data_types_Overviews.py
@@ -0,0 +1,15 @@
+# -----------------------------
+# type()
+# all data in python is object
+# -----------------------------
+
+print(type(10))
+
+print(type(100.9))
+
+print(type("hello python"))
+
+print(type([1, 2, 3, 4, ]))
+
+print(type((1, 2, 3, )))
+print(type(2 == 2))
