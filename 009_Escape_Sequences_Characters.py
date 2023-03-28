71978409d5ed4de2c8222c8195d70d4ccd27dcb7
khoubaib <zombie.1255x@gmail.com>
2023-03-28 09:26:28 +0100

    The vd nomber 9

---
+++ 009_Escape_Sequences_Characters.py
@@ -0,0 +1,28 @@
+# Back Space
+print("Hello\bWorld")
+
+# Escape New Line + \
+print("Hello\
+      I love\
+      Python")
+
+# Escape Back Slash
+print("I Love Back Slash \\")
+
+# Escape Single Quote
+print('I Love Single Quote \'test\' ')
+
+# Escape Double Quaote
+print("I Love Double Quaotes \"Test\" ")
+
+# Line Feed
+print("Hello World\nSecond Line")
+
+# Carriage Return
+print("123456\rAbcde")
+
+# Horizontal Tab
+print("Hello\tPython")
+
+# Character Hex Value
+print("\x4F\x73")
