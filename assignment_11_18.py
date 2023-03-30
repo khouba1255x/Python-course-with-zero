0e2e91bc9d3f0addaa0f6e8999ef7f07c1cb101e
khoubaib <zombie.1255x@gmail.com>
2023-03-30 09:35:48 +0100

    The second assignment

---
+++ assignment_11_18.py
@@ -0,0 +1,104 @@
+
+# 1
+print("assignment 1")
+
+n = "khoubaib"
+a = 17
+c = "tunisia"
+print(
+    f'''Hello '{n}', How You Doing \ """ Your Age Is "{a}"" + And Your Contry Is: {c} ''')
+
+# 2
+print("assignment 2")
+
+n = "khoubaib"
+a = 17
+c = "tunisia"
+print(
+    f'''Hello '{n}', How You Doing \ \n """ Your Age Is "{a}"" + \n And Your Contry Is: {c} ''')
+
+# 3
+print("assignment 3")
+
+name = "Elzero"
+print(f"""Second Letter is "{name[1]}" """)
+print(f"""Third Letter is "{name[2]}" """)
+print(f"""last Letter is "{name[-1]}" """)
+
+# 4
+print("assignment 4")
+
+name = "Elzero"
+print(f""" "{name[1:4]}" """)
+print(f""" "{name[0]+name[2]+name[-2]}" """)
+print(f""" "{name[-2]+name[2]+name[0]}" """)
+
+# 5
+print("assignment 5")
+
+name1 = "#@#@Elzero#@#@"
+print(name1.strip("#@"))
+
+# 6
+print("assignment 6")
+
+num1 = "9"
+num2 = "15"
+num3 = "130"
+num4 = "950"
+num5 = "1500"
+print(num1.zfill(4))
+print(num2.zfill(4))
+print(num3.zfill(4))
+print(num4.zfill(4))
+print(num5.zfill(4))
+
+# 7
+print("assignment 7")
+
+name_one = "Osama"
+name_two = "Osama_Elzero"
+print(name_one.rjust(20, "@"))
+print(name_two.rjust(20, "@"))
+
+# 8
+print("assignment 8")
+
+name_one1 = "OSamA"
+name_two2 = "osaMA"
+print(name_one1.swapcase())
+print(name_two2.swapcase())
+
+# 9
+print("assignment 9")
+
+msg = "I Love Python And Although Love Elzero Web Scholl"
+print(msg.count("Love"))
+
+# 10
+print("assignment 10")
+
+name_1 = "Elzero"
+print(name_1.index("z"))
+
+# 11
+print("assignment 11")
+
+msg1 = "I <3 Python And Althought <3 Elzero Web School"
+print(msg1.replace("<3", "Love", 1))
+
+# 12
+print("assignment 12")
+
+msg1 = "I <3 Python And Althought <3 Elzero Web School"
+print(msg1.replace("<3", "Love"))
+
+# 13
+print("assignment 13")
+
+name = "Osama"
+age = 38
+country = "Egypt"
+print(f"My Name Is {name}, And My Age Is {age}, And My Country Is {country}")
+
+# I finished the tasks
