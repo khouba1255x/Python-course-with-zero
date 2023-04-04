2ce65cd8aab2e19bedaea28c7902ef4ee20e7c6c
khoubaib <zombie.1255x@gmail.com>
2023-04-04 07:01:01 +0100

    The vd nomber 20

---
+++ 020_Arithmetic_Operators.py
@@ -0,0 +1,64 @@
+# --------------------------
+# -- Arithmetic Operators --
+# --------------------------
+# [+] Addition
+# [-] Subtraction
+# [*] Multiplication
+# [/] Division
+# [%] Modulus
+# [**] Exponent
+# [//] Floor Division
+# --------------------------
+
+# Addition
+print("Additon")
+
+print(10 + 30)  # 40
+print(-10 + 20)  # 10
+print(1 + 2.66)  # 3.66
+print(1.2 + 1.2)  # 2.4
+
+# Subtraction
+print("Subtracton")
+
+print(60 - 30)  # 30
+print(-30 - 20)  # -50
+print(-30 - -20)  # -10
+print(5.66 - 3.44)  # 2.22
+
+# Multiplication
+print("Multiplication")
+
+print(10 * 3)  # 30
+print(5 + 10 * 100)  # 1005
+print((5 + 10) * 100)  # 1500
+
+# Division
+print("Division")
+
+print(100 / 20)  # 5.0
+print(int(100 / 20))  # 5
+
+# Modulus
+print("Modulus")
+
+print(8 % 2)
+print(9 % 2)
+print(20 % 5)
+print(22 % 25)
+
+# Exponent
+print("Exponet")
+
+print(2 ** 5)
+print(2 * 2 * 2 * 2 * 2)
+print(5 ** 4)
+print(5 * 5 * 5 * 5)
+
+# Floor Division
+print("Floor Division")
+
+print(100 // 20)
+print(119 // 20)
+print(120 // 20)
+print(139 // 20)
