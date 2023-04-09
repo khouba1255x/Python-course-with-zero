e0de4f3d68026298c5399162ddbb5051ab7619b7
khoubaib <zombie.1255x@gmail.com>
2023-04-09 10:02:36 +0100

    The assignment for vd 24 to 25

---
+++ assignment_24_25.py
@@ -0,0 +1,26 @@
+# 1
+print("assignment 1")
+
+name = "Osama",
+print(name[0])
+print(type(name))
+
+# 2
+print("assignment 2")
+
+friends = ("Osama", "Ahmed", "Sayed")
+friends = ("Elzero",) + friends[1:]
+print(friends)
+
+print(type(friends))
+print(len(friends))
+
+# 3
+print("assignment 3")
+
+nums = (1, 2, 3)
+letters = ("A", "B", "C", )
+c = nums + letters
+
+print(c)
+print(len(c))
