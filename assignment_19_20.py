efb24632c1abf342fe4f3de42595fb9b8c053b01
khoubaib <zombie.1255x@gmail.com>
2023-04-09 07:21:26 +0100

    The assignment for vd 19 to 20

---
+++ assignment_19_20.py
@@ -0,0 +1,58 @@
+# 1
+print("assignment 1")
+
+print(10)
+print(10.55)
+print(15 + 4j)
+
+print(type(10))
+print(type(10.55))
+print(type(15 + 4j))
+
+# 2
+print("assignment 2")
+
+myComplexNumber = 1+2j
+
+print(type(myComplexNumber))
+
+print("Real Part Is: {}".format(myComplexNumber.real))
+print("Imaginary Part Is: {}".format(myComplexNumber.imag))
+
+# 3
+print("assignment 3")
+
+num = 10
+print("The float : {:.10f}".format(num))
+
+# 4 type1
+print("assignment 4 type1")
+
+num1 = 159.650
+# n1 = int(num1)
+print(int(num1))
+print(type(int(num1)))
+# ena 5demt hakka w alahou A3lem ken s7i7 w ella lea
+
+# 4 type1
+print("assignment 4 type2")
+
+num1 = 159.650
+n1 = int(num1)
+print(int(n1))
+print(type(n1))
+
+# 5
+print("assignment 5")
+
+a = 100 - 115
+b = 50 * 30
+c = 21 % 4
+d = 110 // 11
+e = 97 // 20
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
