a2763efea0bba9e68f268bf097c272f4ced1e492
khoubaib <zombie.1255x@gmail.com>
2023-04-09 09:23:37 +0100

    Edite the file and finish the assignment for vd 21 to 23

--- assignment_21_23.py
+++ assignment_21_23.py
@@ -5,10 +5,10 @@ friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
 friends1 = friends.copy()
 
 print(friends[0] + "Method One")
-print(friends.pop(0) + "Method Two")
+print(friends.pop(0) + "Method Two")  # walli shouf chniyya il method 2
 
 print(friends[0] + " Method One")
-print(friends.pop(0) + " Method Two")
+print(friends.pop(0) + " Method Two")  # walli shouf chniyya il method 2
 
 # 2
 print("assignment 2")
@@ -30,3 +30,56 @@ friends1.insert(6, "Elzero")
 friends1.pop(-2)
 friends1.insert(-1, "Elzero")
 print(friends1)
+
+# 5
+print("assignment 5")
+
+friends = ["Osama", "Ahmed", "Sayed"]
+
+friends.insert(0, "Nasser")
+friends.insert(4, "Salem")
+print(friends)
+
+# 6
+print("assignment 6")
+
+friends.pop(1)
+friends.pop(2)
+print(friends)
+
+friends.pop(-1)
+print(friends)
+
+# 7
+print("assignment 7")
+
+friends = ['Nasser', 'Ahmed']
+employees = ["Samah", "Eman"]
+school = ["Ramy", "Shady"]
+
+friends.extend(employees)
+friends.extend(school)
+
+print(friends)
+
+# 8
+print("assignment 8")
+
+friends.sort()
+print(friends)
+
+friends.sort(reverse=True)
+print(friends)
+
+# 9
+print("assignment 9")
+
+print(len(friends))
+
+# 10
+print("assignment 10")
+
+technologies = ["Html", "CSS", "JS", ["Django", "Flask", "Web"]]
+
+print(technologies[3][0])
+print(technologies[3][-1])
