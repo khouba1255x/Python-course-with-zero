7e271de8355a7c491794b0758c6d2e83af9cd725
khoubaib <zombie.1255x@gmail.com>
2023-04-16 06:14:48 +0100

    The vd number 30 , 20.46%

---
+++ 031_Dictionary_Methods_Part1.py
@@ -0,0 +1,42 @@
+# ------------------------
+# -- Dictionary Methods --
+# ------------------------
+
+# clear()
+print("# clear()   :")
+
+user = {
+    "name": "Osama"
+}
+print(user)
+user.clear()
+print(user)
+
+print("=" * 50)
+
+# update ()
+print("# update ()   :")
+
+memeber = {
+    "name": "Osama"
+}
+print(memeber)
+memeber["age"] = 36
+print(memeber)
+memeber.update({"contry": "Egypt"})
+print(memeber)
+
+print("=" * 50)
+
+# copy()
+print("# copy()   :")
+
+main = {
+    "name": "Osama"
+}
+
+b = main.copy()
+print(b)
+main.update({"skills": "Fighting"})
+print(main)
+print(b)
