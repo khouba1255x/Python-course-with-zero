d1b93d2ebbae711cf33e1bf1468dafadbbf1f8cf
khoubaib <zombie.1255x@gmail.com>
2023-05-17 09:40:34 +0100

    The assignment 19 to 20

---
+++ assignment/v2_assignment_19_20.py
@@ -0,0 +1,58 @@
+print("=" * 40)
+# 1 : 
+print("assignment 1: ")
+
+print("int =",10)
+print("float =",10.5)
+print("complex = ",10.5+5j)
+
+print("=" * 40)
+# 2 :
+print("assignment 2: ")
+
+a = 1+2j
+
+print("The imaginary part : {}".format(a.imag))
+print("The Real part : {}".format(a.real))
+
+print("=" * 40)
+# 3 :
+print("assignment 3:")
+
+num = 10 
+
+print("The output = {:.10f}".format(num))
+
+print("=" *40)
+# 4 : 
+print("assignment 4: ")
+
+print("=" * 10)
+#type 1 :
+print("type 1 :")
+
+num = 159.650
+
+num = int(num)
+print(num)
+print(type(num))
+
+print("=" * 10)
+# type 2 :
+print("type 2 :")
+
+num = 159.650
+
+print("The int of {:.2f}  = {:d}".format(num, int(num)))
+
+print("=" *40)
+# 5 : 
+print("assignment 5 :")
+
+print(100 - 115)
+print(50 * 30)
+print(21 % 4)
+print(110 // 11)
+print(97 // 20)
+
+# finish
