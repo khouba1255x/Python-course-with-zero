c9d2ce43eeb6d67a986f75e3219b155b3a78467a
khoubaib <zombie.1255x@gmail.com>
2023-05-17 09:38:29 +0100

    small edit

--- assignment/assignment_19_20.py
+++ assignment/assignment_19_20.py
@@ -1,3 +1,4 @@
+print("=" * 40)
 # 1
 print("assignment 1")
 
@@ -9,6 +10,7 @@ print(type(10))
 print(type(10.55))
 print(type(15 + 4j))
 
+print("=" * 40)
 # 2
 print("assignment 2")
 
@@ -19,12 +21,14 @@ print(type(myComplexNumber))
 print("Real Part Is: {}".format(myComplexNumber.real))
 print("Imaginary Part Is: {}".format(myComplexNumber.imag))
 
+print("=" * 40)
 # 3
 print("assignment 3")
 
 num = 10
 print("The float : {:.10f}".format(num))
 
+print("=" * 40)
 # 4 type1
 print("assignment 4 type1")
 
@@ -34,6 +38,7 @@ print(int(num1))
 print(type(int(num1)))
 # ena 5demt hakka w alahou A3lem ken s7i7 w ella lea
 
+print("=" * 40)
 # 4 type2
 print("assignment 4 type2")
 
@@ -42,6 +47,7 @@ n1 = int(num1)
 print(int(n1))
 print(type(n1))
 
+print("=" * 40)
 # 4 type3
 print("assignment 4 type3")
 
@@ -49,6 +55,7 @@ num1 = 159.650
 print("the nubmer int  {:d}".format(int(num1)))
 print("the nubmer int  {}".format(type(int(num1))))
 
+print("=" * 40)
 # 5
 print("assignment 5")
 
