0b11926dcb39cbc00cf9984977ca54ff9a889467
khoubaib <zombie.1255x@gmail.com>
2023-05-19 09:13:50 +0100

    The vd number 33

---
+++ course/033_Boolean.py
@@ -0,0 +1,37 @@
+# -------------
+# -- Boolean --
+# -------------
+# [1] In Programming You Need to Known Your If Your Code Output is True Or False
+# [2] Boolean Values Are The Two Constant Objects False + True.
+# ---------------------------------------------------------------
+
+print("=" * 40)
+# introduction :
+print("# introduction :")
+
+name = " "
+print(name.isspace())
+
+print(100 > 200)
+print(100 > 100)
+print(100 > 90)
+
+print("=" * 40)
+# True Values :
+print("# True Values :")
+
+print(bool("Osama"))
+print(bool(100))
+print(bool(100.5))
+print(bool(True))
+print(bool([1, 2, 3, 4, 5]))
+
+print("=" * 40)
+# False Values :
+print("# False Values :")
+
+print(bool(""))
+print(bool())
+print(bool(0))
+print(bool(False))
+print(bool([]))
