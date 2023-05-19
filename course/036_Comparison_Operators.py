11929b5000edb0c0df13cebee8207c5298c2db2e
khoubaib <zombie.1255x@gmail.com>
2023-05-19 10:40:19 +0100

    The vd number 36

---
+++ course/036_Comparison_Operators.py
@@ -0,0 +1,56 @@
+# --------------------------
+# -- Comparison Operators --
+# --------------------------
+# [ == ] Equal
+# [ != ] Not Equal
+# [ > ] Greater Than
+# [ < ] Less Than
+# [ >= ] Greater Than Or Equal
+# [ <= ] Less Than Or Equal
+# --------------------------
+
+print("=" * 40)
+# Equal + Not Equal :
+print("# Equal + Not Equal :")
+
+print(100 == 100)
+print(100 == 200)
+print(100 == 100.00)
+
+print("=" * 10)
+
+print(100 != 100)
+print(100 != 200)
+print(100 != 100.00)
+
+print("=" * 40)
+# Greater Than + Less Than :
+print("# Greater Than + Less Than :")
+
+print(100 > 100)
+print(100 > 200)
+print(100 > 100.00)
+print(100 > 40)
+
+print("=" * 10)
+
+print(100 < 100)
+print(100 < 200)
+print(100 < 100.00)
+print(100 < 40)
+
+print("=" * 40)
+# Greater Than or Equial + Less Than or Equial :
+print("# Greater Than or Equial + Less Than or Equial :")
+
+print(100 >= 100)
+print(100 >= 200)
+print(100 >= 100.00)
+print(100 >= 40)
+
+print("=" * 10)
+
+print(100 <= 100)
+print(100 <= 200)
+print(100 <= 100.00)
+print(100 <= 40)
