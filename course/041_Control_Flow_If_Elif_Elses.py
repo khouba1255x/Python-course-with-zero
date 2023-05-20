e7315dd552379b0103b362bda2d8b7ac5ce3da90
khoubaib <zombie.1255x@gmail.com>
2023-05-20 09:54:24 +0100

    The vd number 41

---
+++ course/041_Control_Flow_If_Elif_Elses.py
@@ -0,0 +1,26 @@
+# --------------------
+# --  Control Flow  --
+# -- If, Elif, Else --
+# -- Make Decisions --
+# --------------------
+
+uName = "Osama"
+uCountry = "Kuwait"
+cName = "Python Course"
+cPrice = 100
+
+if uCountry == "Egypt" :
+    print(f"Hello {uName} Because You Are From {uCountry}")
+    print(f"The Course \"{cName}\" Price is: ${cPrice - 80}")
+
+elif uCountry == "KSA" :
+    print(f"Hello {uName} Because You Are From {uCountry}")
+    print(f"The Course \"{cName}\" Price is: ${cPrice - 60}")
+    
+elif uCountry == "Kuwait" :
+    print(f"Hello {uName} Because You Are From {uCountry}")
+    print(f"The Course \"{cName}\" Price is: ${cPrice - 50}")
+    
+else :
+    print(f"Hello {uName} Because You Are From {uCountry}")
+    print(f"The Course \"{cName}\" Price is: ${cPrice - 30}")
