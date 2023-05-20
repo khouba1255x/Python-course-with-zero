8205bf4ed99ef7eea9e537e7f7e51a7cca291055
khoubaib <zombie.1255x@gmail.com>
2023-05-20 15:21:20 +0100

    small edit

--- tojraba/046_Practical_Membership_Control_my_work.py
+++ tojraba/046_Practical_Membership_Control_my_work.py
@@ -2,7 +2,37 @@
 # -- Practical Membership Control --
 # ----------------------------------
 
-print("version 1:" .center(50, "#"))
+print(" version 1: " .center(50, "#"))
+
+print("=" * 40)
+# # List Contains Admins :
+# admins = ["Ahmed", "Osama", "Sameh", "Manel", "Rahma", "Mahmoud"]
+
+# # Login : 
+# name = input("Please Type YOur Name : ").strip().capitalize()
+
+# # if Name is in admin :
+
+# if name in admins : 
+#     print(f"Hello {name} Welcome Back")
+    
+#     option = input("Delete or Update Your Name ? : ").strip().capitalize()
+    
+#     if option == "Update" :
+#         TheNewName = input("Your New Name Please : ").strip().capitalize()
+        
+#         admins.remove(name)
+#         admins.append(TheNewName)
+#     print(admins)
+# else : 
+#     print("You are not Admin")
+    
+
+
+
+
+
+print(" version 2: " .center(50, "#"))
 
 print("=" * 40)
 # List Contains Admins :
@@ -21,8 +51,7 @@ if name in admins :
     if option == "Update" :
         TheNewName = input("Your New Name Please : ").strip().capitalize()
         
-        admins.remove(name)
-        admins.append(TheNewName)
+        admins[admins.index(name)] = TheNewName
     print(admins)
 else : 
     print("You are not Admin")
