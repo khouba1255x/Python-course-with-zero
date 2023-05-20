6da2eff9408b58dbe65438aac9e1f5ae6538541d
khoubaib <zombie.1255x@gmail.com>
2023-05-20 10:31:37 +0100

    The vd number 43

---
+++ course/043_Control_Flow_Ternary_Conditional_Operator.py
@@ -0,0 +1,28 @@
+# ----------------------------------
+# -- Ternary Conditional Operator --
+# ----------------------------------
+print("=" * 40)
+
+country = "Egypt"
+
+if country == "Egypt" : print(f"The Weather in {country} is 15")
+
+elif country == "KSA" : print(f"The Weather in {country} is 30")
+
+else : print("Country is not in the list")
+
+print("=" * 40)
+# Short if :
+print("# Short if :")
+
+movieRate = 18
+age = 18
+
+if age < movieRate :
+    print("Movie is Not Good 4u") # Condition if True
+    
+else : 
+    print("Movie is Good 4u and Happy Watching") # Condition if False
+    
+
+print("Movie is Not Good 4u" if age < movieRate else "Movie is Good 4u and Happy Watching")
