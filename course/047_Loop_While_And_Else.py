cc776e9734011b9beee7a5cf121dadc95995c810
khoubaib <zombie.1255x@gmail.com>
2023-05-21 11:56:07 +0100

    The vd number 47

---
+++ course/047_Loop_While_And_Else.py
@@ -0,0 +1,17 @@
+# -------------------
+# -- Loop => While --
+# -------------------
+# while condition_is_true
+#   Code Will Run Until Condition Become False
+# -----------------------
+
+a = 0
+
+while a < 15 : 
+    print(a)
+    a += 1
+
+else : print("Loop is Done") # True Become False
+
+while False :
+    print("Will Not Print")
