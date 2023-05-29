c255c999595ced92ebb30bfd1219baf34da3144f
khoubaib <zombie.1255x@gmail.com>
2023-05-29 02:12:12 +0100

    The assignment 60 to 64

---
+++ assignment/assignment_60_64.py
@@ -0,0 +1,55 @@
+print("=" * 40)
+# assignment 1:
+print("# assignment 1:")
+
+# def get_score(**subjects) :
+#     i = 0
+#     for subject_name, subject_value in subjects.items() :
+#         print(i)
+#         i = i +1
+#         print(f"{subject_name} => {subject_value}")
+
+# print(get_score(Maht = 90, Science = 80, Language = 70))
+
+print("=" * 40)
+# assignment 2:
+print("# assignment 2:")
+
+# # function
+# def get_people_scores(name, **subjects) :
+            
+#     if len(subjects) > 0 :
+        
+#         print(f"Hello {name} This is Your Score Table:")
+        
+#         for subject_name, subject_value in subjects.items() :
+#             print(f"{subject_name} => {subject_value}")
+    
+#     elif len(subjects) == 0 and name == str(name) :
+#         print(f"Hello {name} You Have No Scores To Show")
+
+# # inputs
+# input_name = lambda: input("Type your name : ").strip().capitalize()
+# how_many_subject = lambda: int(input("Type How many Subject You done : "))
+
+# name = input_name()
+# subject_number = how_many_subject()
+
+# subject = {}
+
+# for number in range (subject_number) :
+    
+#     subject_name = input(f"Subject Name {number +1} : ").strip().capitalize()
+#     subject_note = int(input(f"Note of {subject_name} = "))
+    
+#     subject.update({subject_name: subject_note})
+#     print(subject)
+
+# # call function 
+# get_people_scores(name, **subject)
+
+print("=" * 40)
+# assignment 3:
+print("# assignment 3:")
+
+# kif elli fet w ashil mennou mosh lezim nektbou
