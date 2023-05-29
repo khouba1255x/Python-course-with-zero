41d4fccd37b2d6816b7181d0108ff1b5fff4be5d
khoubaib <zombie.1255x@gmail.com>
2023-05-29 13:18:54 +0100

    The vd number 68

---
+++ course/068_Files_Handling_Part4_Importanr_Info.py
@@ -0,0 +1,20 @@
+# -------------------------------------
+# -- File Handling => Important Info --
+# -------------------------------------
+
+print("=" * 40)
+# myFile = open(r"C:\khouba\Elzero\Python\Python-course-with-zero\course\osama.txt", "a")
+# myFile.truncate(5)
+
+print("=" * 40)
+# myFile = open(r"C:\khouba\Elzero\Python\Python-course-with-zero\course\osama.txt", "a")
+# print(myFile.tell())
+
+print("=" * 40)
+# myFile = open(r"C:\khouba\Elzero\Python\Python-course-with-zero\course\osama.txt", "r")
+# myFile.seek(11)
+# print(myFile.read())
+
+import os
+
+os.remove(r"C:\khouba\Elzero\Python\Python-course-with-zero\course\osama.txt")
