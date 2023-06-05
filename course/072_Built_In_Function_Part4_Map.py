ab538766e6ed9ba928d195c703750b64ebe359ac
khoubaib <zombie.1255x@gmail.com>
2023-06-05 10:34:25 +0100

    The vd number 72

---
+++ course/072_Built_In_Function_Part4_Map.py
@@ -0,0 +1,31 @@
+# -------------------------------
+# -- Built In Functions => Map --
+# -------------------------------
+# [1] Map Take A Function + Iterator
+# [2] Map Called Map Because It Map The Function On Every Element
+# [3] The Function Can Be Pre-Defined Function or Lambda Function
+# ---------------------------------------------------------------
+print("=" * 40)
+# Use Map With Predefined Function
+print("# Use Map With Predefined Function")
+
+def formatText(text) :
+    
+    return f"- {text.strip().capitalize()} -"
+
+myTexts = ["OSama", "AHMED", "  sAYed  "]
+
+myFormateData = map(formatText, myTexts)
+
+print(myFormateData)
+
+# print type 1
+for name in myFormateData :
+    print(name)
+
+print("=" * 10)
+
+# print type 2
+for name in list(map(formatText, myTexts)) :
+    print(name)
+
