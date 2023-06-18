87caf23cb75a1eb1f70caed2654c164648e32da1
khoubaib <zombie.1255x@gmail.com>
2023-06-18 18:30:02 +0100

    The vd number 89

---
+++ course/089_Installing_And_Use_pylint_For_Better_Code.py
@@ -0,0 +1,18 @@
+# -----------------------------------------------
+# -- Installing And Use Pylint For Better Code --
+# -----------------------------------------------
+print("=" * 40)
+
+"""
+This is My Module
+To Create Function
+To Say Hello
+"""
+
+def say_hello(name) :
+    
+    msg = "Hello"
+    
+    return f"{msg} {name}"
+
+say_hello("Ahmed")
