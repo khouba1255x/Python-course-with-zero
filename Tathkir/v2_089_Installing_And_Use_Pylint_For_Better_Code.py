dfb65ef28f43dd88e3a82b7eb66eb6827cfba8cd
khoubaib <zombie.1255x@gmail.com>
2023-09-08 15:22:09 +0100

    The vd v2 89 , ena kammaltou ama jarribt baarsha 7ouloul w houwwa mea7abbitsh ye5dem il faza mta3 il raiting kol meansalli7 7aja y9olli raw 0/10

---
+++ Tathkir/v2_089_Installing_And_Use_Pylint_For_Better_Code.py
@@ -0,0 +1,10 @@
+# -----------------------------------------------
+# -- Installing And Use Pylint For Better Code --
+# -----------------------------------------------
+
+def sayHello (name) :
+    
+    msg = "Hello"
+    return f"{msg} {name}"
+
+sayHello("Ahmed")
