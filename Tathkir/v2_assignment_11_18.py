fe017538a1454cb0e22b159c8898ae95599442a2
khoubaib <zombie.1255x@gmail.com>
2023-09-20 10:40:26 +0100

    v2 assignment 11 to 18

---
+++ Tathkir/v2_assignment_11_18.py
@@ -0,0 +1,113 @@
+print("=" * 40)
+# assignment 1: 
+print("# assignment 1: ")
+
+print(""""Hello 'Osama', How You Doing \ "" Your Age Is "38 "" + And Your Country Is: Egypt """)
+
+print("=" * 40)
+# assignment 2: 
+print("# assignment 2: ")
+
+print(""""Hello 'Osama', How You Doing \ \n"" Your Age Is "38 "" + \nAnd Your Country Is: Egypt """)
+
+print("=" * 40)
+# assignment 3: 
+print("# assignment 3: ")
+
+name = "Elzero"
+
+print(f"Second Letter {name[1]}")
+print(f"Third Letter {name[2]}")
+print(f"Last Letter {name[-1]}")
+
+print("=" * 40)
+# assignment 4: 
+print("# assignment 4: ")
+
+# bish nesta3mel il variable illifet  name
+
+print(name[1:-2])
+print(name[::2])
+print(name[-2::-2])
+
+print("=" * 40)
+# assignment 5: 
+print("# assignment 5: ")
+
+name = "#@#@Elzero#@#@"
+print(name.strip("#@"))
+
+print("=" * 40)
+# assignment 6: 
+print("# assignment 6: ")
+
+num1 = "9"
+num2 = "15"
+num3 = "130"
+num4 = "950"
+num5 = "1500"
+
+print(num1.zfill(4))
+print(num2.zfill(4))
+print(num3.zfill(4))
+print(num4.zfill(4))
+print(num5.zfill(4))
+
+print("=" * 40)
+# assignment 7: 
+print("# assignment 7: ")
+
+name_one = "Osama"
+name_two = "Osama_Elzero"
+
+print(name_one.rjust(20, "@"))
+print(name_two.rjust(20, "@"))
+
+print("=" * 40)
+# assignment 8: 
+print("# assignment 8: ")
+
+name_one = "OSamA"
+name_two = "osaMA"
+
+print(name_one.swapcase())
+print(name_two.swapcase())
+
+print("=" * 40)
+# assignment 9: 
+print("# assignment 9: ")
+
+msg = "I Love Python And Although Love Elzero Web School"
+print(msg.count("Love"))
+
+print("=" * 40)
+# assignment 10: 
+print("# assignment 10: ")
+
+name = "Elzero"
+print(name.index("z"))
+
+print("=" * 40)
+# assignment 11: 
+print("# assignment 11: ")
+
+msg = "I <3 Python And Although <3 Elzero Web School"
+
+print(msg.replace("<3", "Love", 1))
+
+print("=" * 40)
+# assignment 12: 
+print("# assignment 12: ")
+
+msg = "I <3 Python And Although <3 Elzero Web School"
+print(msg.replace("<3", "Love"))
+
+print("=" * 40)
+# assignment 13: 
+print("# assignment 13: ")
+
+name = "Osama"
+age = 38
+country = "Egypt"
+
+print(f"My Name Is {name}, And My Age Is {age}, And My Country Is {country} ")
