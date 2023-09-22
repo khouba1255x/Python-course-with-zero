2ef5e21ffb13b634e9011adfe06d6c697536803e
khoubaib <zombie.1255x@gmail.com>
2023-09-22 10:43:40 +0100

    assignment v2 19 to 20

---
+++ Tathkir/v2_assignment_19_20.py
@@ -0,0 +1,31 @@
+print("=" * 40)
+# assignment 2 :
+print("# assignment 2 :")
+
+a = 1 + 2j
+print(a.real)
+print(a.imag)
+
+print("=" * 40)
+# assignment 3 :
+print("# assignment 3 :")
+
+num = 10
+
+print("{:.10f}".format(num))
+
+print("=" * 40)
+# assignment 4 :
+print("# assignment 4 :")
+
+num = 159.650
+
+num1 = int(num)
+print(num1)
+print(type(num1))
+
+print("=" * 40)
+# assignment 5 :
+print("# assignment 5 :")
+
+# sehil mosh leazim ne5dmou
